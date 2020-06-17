5d75a0f49f461a91176d1b5409026b3266219b6b
jdeleonardis <jdeleonardis@hotmail.com>
2020-06-17 10:05:21 -0400

    Changed 'events' model to 'calendar'.  Changed api_routes to point to new calendar model, and added st udent api routes.  Changed html_routes to point to the correct html files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,49 +12,87 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all events
-  app.get("/api/events", function(req, res) {
+  // GET route for getting all calendar events
+  app.get("/api/calendar", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Events.findAll({}).then(function(dbEvents) {
-      // console.log(dbEvents)
+    db.Calendar.findAll({}).then(function(dbCalendar) {
+      // console.log(dbCalendar)
       // We have access to the events as an argument inside of the callback function, but not needed
-      res.json(dbEvents);
+      res.json(dbCalendar);
+    });
+  });
+
+  // GET route for getting all students
+  app.get("/api/students", function(req, res) {
+    db.Student.findAll({}).then(function(dbStudents) {      
+      res.json(dbStudents);
+    });
+  });  
+
+  // GET route for getting one student
+  app.get("/api/students/:id", function(req, res) {
+    db.Student.findOne({
+      where: {
+        id: req.params.id
+      }      
+    }).then(function(dbStudent) {      
+      res.json(dbStudent);
     });
   });
 
   // POST route for saving a new event
-  app.post("/api/event", function(req, res) {
+  app.post("/api/calendar", function(req, res) {
     // console.log(req.body.start)
     // console.log(req.body.end)
-    db.Events.create({
+    db.Calendar.create({
       title: req.body.title,
       description: req.body.description,
       start: req.body.start,
       end: req.body.end
-    }).then(function(dbEvent) {
+    }).then(function(dbCalendar) {
       // We have access to the new event as an argument inside of the callback function
-      res.json(dbEvent);
+      res.json(dbCalendar);
     });
   });  
 
+  // POST route for saving a new student
+  app.post("/api/student", function(req, res) {
+    db.Student.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name
+    }).then(function(dbStudent) {      
+      res.json(dbStudent);
+    });
+  });   
+
   // DELETE route for deleting an event.
-  app.delete("/api/event/:id", function(req, res) {
+  app.delete("/api/calendar/:id", function(req, res) {
     // Use the sequelize destroy method to delete a record from our table with the
     // id in req.params.id. res.json the result back to the user
-    db.Events.destroy({
+    db.Calendar.destroy({
       where: {
         id: req.params.id
       }
     }).then(function() {
       res.end();
     })
-
   });
 
+  // DELETE route for deleting a student.
+  app.delete("/api/student/:id", function(req, res) {
+    db.Student.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function() {
+      res.end();
+    })
+  });  
+
   // PUT route for updating an event.
-  app.put("/api/event", function(req, res) {
+  app.put("/api/calendar", function(req, res) {
             
-    db.Events.update({
+    db.Calendar.update({
       title: req.body.title,
       description: req.body.description,
       start: req.body.start,
@@ -64,9 +102,25 @@ module.exports = function(app) {
       where: {
         id: req.body.id
       }
-    }).then(function(dbEvent) {
+    }).then(function(dbCalendar) {
       // We have access to the new event as an argument inside of the callback function
-      res.json(dbEvent);
+      res.json(dbCalendar);
     });
-   });
+  });
+
+  // PUT route for updating a student.
+  app.put("/api/student", function(req, res) {
+            
+    db.Student.update({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name
+    }, 
+    {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbStudent) {      
+      res.json(dbStudent);
+    });
+  });
 };
