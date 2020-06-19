ff670ea623c01003703dbec93aaf83df2f31678f
rodgersea <elliottarodgers@gmail.com>
2020-06-18 21:18:35 -0400

    test commit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,24 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+var express = require("express");
+var db = require('../models/students.js');
 
-module.exports = function(app) {
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+var router = express.Router();
+
+
+  
+  router.get("/", function(req, res) {
+    db.all(function(data) {
+      var hbsObject = {
+        Students: data
+      };
+      console.log('hbsObject', hbsObject);
+      res.render("index", hbsObject);
+    });
   });
 
-  app.get("/manage", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/manage.html"));
-  });
-};
+  // app.get("/manage", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../public/manage.html"));
+  // });
+  
+module.exports = router;
