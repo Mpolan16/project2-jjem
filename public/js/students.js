8e542616a3cee253c9818a68dfaf3f236a2b5f3e
Nikita <jrringler13759@gmail.com>
2020-06-20 11:07:16 -0400

    delete stu modal

--- public/js/students.js
+++ public/js/students.js
@@ -1,83 +1,72 @@
+$(document).ready(function() {
+
+
+
 const firstName = $("#firstName");
 const lastName = $("#lastName");
 const deleteFirst = $("#dfirstName");
 const deleteLast = $("#dlastName");
+const stuList = $(".deleteStu");
+
+console.log(stuList.length);
 
 
-$(document).on("click", "addBtn", );
-$(document).on("click", "deleteBtn", );
+// var addBtn = document.getElementById("addBtn");
+//         addBtn.addEventListener("click", handleAddStudent);
 
-function handleAddStudent(event) {
+stuList.on("click", handleDeleteStudent);
+
+        
+//$(document).on("click", "#deleteBtn", handleDeleteStudent);
+
+function handleAddStudent() {
     event.preventDefault();
     // Don't do anything if the name fields hasn't been filled out
     if (!firstName.val().trim().trim() || !lastName.val().trim().trim() ) {
       return;
     }
+    
     // Calling the upsertStudent function and passing in the value of the name input
     upsertStudent(
         {
             first_name: firstName
                 .val()
                 .trim()
-        },
-        {
+        ,
+        
             last_name: lastName
                 .val()
                 .trim()
         }
+       
     );
   }
 
   // A function for creating a student
-<<<<<<< HEAD
+
 function upsertStudent(studentData) {
-$.post("/api/student", studentData)
+    console.log(studentData);
+  $.post("/api/student", studentData)
 }
+
+//when close is clicked fields are cleared
+
 
 
 function handleDeleteStudent(event) {
+    console.log(event);
 event.preventDefault();
 // Don't do anything if the name fields hasn't been filled out
-if (!firstName.val().trim().trim() || !lastName.val().trim().trim() ) {
-    return;
+var stuID = $(this).attr("id");
+$.ajax({
+    method: "DELETE",
+    url: "/api/student/" + stuID
+  })
+
 }
 
-deleteStudent(
-    
 
-);
-}
-=======
-  function upsertStudent(studentData) {
-    $.post("/api/student", studentData)
-  }
-
-
-  function handleDeleteStudent(event) {
-    event.preventDefault();
-    // Don't do anything if the name fields hasn't been filled out
-    if (!firstName.val().trim().trim() || !lastName.val().trim().trim() ) {
-      return;
-    }
-    
-    deleteStudent(
-        
-    
-    );
-  }
->>>>>>> b64c1e7e6e44a6106d4be7eae4eda11cdf3d32b2
-
-
-function deleteStudent(studentData) {
-    studentData.stopPropagation();
-    var id = $(this).data("id");
-    $.ajax({
-      method: "DELETE",
-      url: "/api/students/" + id
-    })
-  }
-
-
+})
 
 $('#modal1').modal();
 $('#modal2').modal();
