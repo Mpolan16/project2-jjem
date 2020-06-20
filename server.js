4a7fd3648c6015449d3034ff918c86430dfc20f3
Nikita <jrringler13759@gmail.com>
2020-06-20 09:32:46 -0400

    connection

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.JAWSDB_URL || 8080;
+var PORT = process.env.PORT || 8080;
 
 
 
